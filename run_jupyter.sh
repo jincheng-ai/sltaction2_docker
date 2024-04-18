ac13f0292ee2049c03da8a60d899edefc2bd6405
Jincheng Shu <ai_jcshu@163.com>
2024-04-18 16:47:42 +0800

    暂停1s，等待上一条命令完成

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+docker stop slt
+sleep 1
+docker rm slt
+sleep 1
 docker run -d -p 8888:8888 --gpus "device=0" --shm-size 8G \
 	--name slt jincheng_jcs/mmaction2:main \
 	jupyter notebook --ip 0.0.0.0 --no-browser --allow-root
@@ -7,5 +11,5 @@ docker cp -L ./sltaction2 slt:/workspace/
 docker cp -L ./data slt:/workspace/
 docker cp -L ./weights slt:/workspace/
 docker cp -L ./outputs slt:/workspace/
-
-docker exec slt jupyter server list
+sleep 1
+docker exec slt jupyter notebook list
